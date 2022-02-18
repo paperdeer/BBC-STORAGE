9bfbabbb8bc7569500d6be1fd403a2535e540b34
강석현 <tjrgus0713@gmail.com>
2022-02-18 16:17:06 +0900

    ⚡️:: store에 미들웨어 추가

--- src/modules/redux/store/index.ts
+++ src/modules/redux/store/index.ts
@@ -1,7 +1,13 @@
-import {createStore} from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from '../reducer/index'
+import createSagaMiddleware from "@redux-saga/core";
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootSaga from "../middleware";
 
-const store = createStore(rootReducer);
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
+
+sagaMiddleware.run(rootSaga);
 
 export type ReducerType = ReturnType<typeof rootReducer>;
 export default store;
