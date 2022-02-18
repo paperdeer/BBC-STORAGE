51f0bd8acc958af990b5d7693d14e32c54674483
강석현 <tjrgus0713@gmail.com>
2022-02-18 16:09:03 +0900

    ⚡️:: 코드 스타일 정리 및 필요없는 파일 삭제

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -1,35 +1,35 @@
-import React, {ChangeEvent, FormEvent, useState} from 'react';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
 import * as S from './styles';
-import {useDispatch, useSelector} from "react-redux";
-import {reducerType} from "../../modules/redux/reducer";
-import {setOnSearch} from "../../modules/redux/action/search";
-import {useNavigate} from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { reducerType } from "../../modules/redux/reducer";
+import { setOnSearch } from "../../modules/redux/action/search";
+import { useNavigate } from "react-router-dom";
 import HistoryList from "./history-list";
 
-const Search:React.FC = () => {
-    const [searchKeyword,setSearchKeyword] = useState('');
-    const [onFocusInput,setOnFocusInput] = useState(false);
+const Search: React.FC = () => {
+    const [searchKeyword, setSearchKeyword] = useState('');
+    const [onFocusInput, setOnFocusInput] = useState(false);
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    const searchHistoryList = useSelector((state:reducerType) => state.search.searchHistoryList)
-    const onChangeSearchKeyword = (e:ChangeEvent<HTMLInputElement>) => {
+    const searchHistoryList = useSelector((state: reducerType) => state.search.searchHistoryList)
+    const onChangeSearchKeyword = (e: ChangeEvent<HTMLInputElement>) => {
         setSearchKeyword(e.target.value)
     }
     const onClickSearchButton = () => {
         dispatch(setOnSearch(searchKeyword))
-        localStorage.setItem("searchHistoryList",JSON.stringify(searchHistoryList.concat(searchKeyword)))
+        localStorage.setItem("searchHistoryList", JSON.stringify(searchHistoryList.concat(searchKeyword)))
     }
-    const onSubmitSearchKeyword = (e:FormEvent<HTMLFormElement>) => {
+    const onSubmitSearchKeyword = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        navigate("/search?username="+searchKeyword);
+        navigate("/search?username=" + searchKeyword);
         onClickSearchButton()
     }
     return (
         <S.Wrapper onSubmit={onSubmitSearchKeyword}>
             <label>
-                <input onChange={onChangeSearchKeyword} value={searchKeyword} onFocus={()=>setOnFocusInput(true)} onBlur={()=>setOnFocusInput(false)} />
+                <input onChange={onChangeSearchKeyword} value={searchKeyword} onFocus={() => setOnFocusInput(true)} onBlur={() => setOnFocusInput(false)} />
             </label>
-            {onFocusInput && <HistoryList/>}
+            {onFocusInput && <HistoryList />}
         </S.Wrapper>
     )
 }
--- src/modules/redux/action/userInfo/interface/index.ts
+++
@@ -1,2 +0,0 @@
-export const GET_SUMMONER = 'userInfo/GET_SUMMONER' as const;
-export const GET_LEAGUE = 'userInfo/GET_LEAGUE' as const;
