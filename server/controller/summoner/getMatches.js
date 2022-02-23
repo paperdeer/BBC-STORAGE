8848881e217416bdc5dd79ef24f098c6ca1fcb58
강석현 <tjrgus0713@gmail.com>
2022-02-24 01:43:31 +0900

    ⚡️:: match 정보를 주는 api 추가

--- server/controller/summoner/getMatches.js
+++ server/controller/summoner/getMatches.js
@@ -10,10 +10,20 @@ const getMatchId = async (puuid) => {
             console.log(err)
         })
 }
-const getMatchDetails = async (matchIdArray) => {
+const getMatchDetails = async (matchIdArray, puuid) => {
     const promises = matchIdArray.map(async id => {
         return await await axios.get(`https://asia.api.riotgames.com/lol/match/v5/matches/${id}?api_key=${API_KEY}`)
-            .then((res) => { return res.data })
+            .then(async (res) => {
+                const participant = await res.data.info.participants.filter((item, index) => {
+                    if (item.puuid === puuid) return item
+                })
+                return {
+                    game_mode: res.data.info.gameMode,
+                    match_id: res.data.metadata.matchId,
+                    duration: res.data.info.gameDuration,
+                    participant: participant[0]
+                }
+            })
     })
     const matches = await Promise.all(promises);
     return matches
@@ -21,8 +31,8 @@ const getMatchDetails = async (matchIdArray) => {
 module.exports.getMatchInfo = async (req, res) => {
     try {
         const { puuid } = req.query
-        const matchId = await getMatchId(puuid);
-        const matches = await getMatchDetails(matchId)
+        const matchIds = await getMatchId(puuid);
+        const matches = await getMatchDetails(matchIds, puuid)
         res.send({
             matches: matches
         })
